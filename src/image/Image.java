8184238eb3cd30b6402a45309389df367ae2fa18
Melliti ramla <ram.melliti@gmail.com>
2020-01-27 14:28:03 +0100

    27/01

--- src/image/Image.java
+++ src/image/Image.java
@@ -4,7 +4,7 @@ package image;
 public class Image {
 
     public static void main(String[] args) {
-        int i,j;
+        
         
         int Matrice[][]=new int [][]{
                                 {25,28,100,100,100,100},
@@ -12,89 +12,60 @@ public class Image {
                                 {65,65,50,20,100,100},
                                 {90,62,100,5,0,0},
                                 {60,87,100,0,100,100}   };
-        for(i=0;i<5;i++){
-            System.out.println();
-            for(j=0;j<6;j++){
-                System.out.print(Matrice[i][j]+"\t");
-            }
-        }
+        affiche(Matrice);
         ///*********Affichage de nuance inversé ***********///
          System.out.print("\n Nuance inversé :");
          int [][] matInv=Mystere(Matrice);
-         for(int k=0;k<5;k++){
-            System.out.print("\n");
-            for(int l=0;l<6;l++){
-                System.out.print(matInv[k][l]+"\t");
-            }
-        }
-         ///////********** Affichage moyenne******************************************
+         affiche(matInv);
+           ///////********** Affichage moyenne******************************************
          System.out.print("\n La saturation moyenne vaut : "+Moyenne(Matrice));
          ///**********Affichage de matrice avec les Contrastes diminués ******
          System.out.print("\n Contrast Diminué :");
          int [][] matDim=DiminuantContrast(Matrice);
-          for(i=0;i<5;i++){
-            System.out.println();
-            for(j=0;j<6;j++){
-                System.out.print(matDim[i][j]+"\t");
+         affiche(matDim);
+    }
+        public static void affiche(int[][] M){
+             for(int i=0;i<5;i++){
+               System.out.println();
+               for(int j=0;j<6;j++){
+                    System.out.print(M[i][j]+"\t");
+                }
             }
         }
-    }
-    
-    
+        
+   
     //////**************** fonction Mystere *************************
     public static int[][] Mystere(int [][]M){
-        
-        //int [][] matrice1=new int[5][6];
-         int matrice1[][]=new int [][]{
-                                {25,28,100,100,100,100},
-                                {44,45,50,100,100,100},
-                                {65,65,50,20,100,100},
-                                {90,62,100,5,0,0},
-                                {60,87,100,0,100,100}   };
+    
          for(int i=0;i<5;i++){
             for(int j=0;j<6;j++){
-                matrice1[i][j]=100-matrice1[i][j];
+                M[i][j]=100-M[i][j];
             }
         }
-        return matrice1;
+        return M;
     }
     
     //////***************** fonction Moyenne **********************
-    public static double Moyenne(int[][]M){
-     int matrice1[][]=new int [][]{
-                                {25,28,100,100,100,100},
-                                {44,45,50,100,100,100},
-                                {65,65,50,20,100,100},
-                                {90,62,100,5,0,0},
-                                {60,87,100,0,100,100}   };
+    public static int Moyenne(int[][]M){
+     
      int som=0;
-     double moy;
+     int moy;
      for(int i=0;i<5;i++){
             for(int j=0;j<6;j++){
-                som=som+matrice1[i][j];
+                som=som+M[i][j];
             }
     }
-     moy=som/30.0;
+     moy=som/30;
      return moy;
     }
     
     /////////////*******************Fonction Diminuant Contrast*******************
-    public static int[][] DiminuantContrast(int [][]M){
-        
-        int matrice2[][]=new int [][]{
-                                {25,28,100,100,100,100},
-                                {44,45,50,100,100,100},
-                                {65,65,50,20,100,100},
-                                {90,62,100,5,0,0},
-                                {60,87,100,0,100,100}   };
-        
-    
+    public static int[][] DiminuantContrast(int [][]matrice2){
+        int moy=Moyenne(matrice2); int correction; 
          for(int i=0;i<5;i++){
             for(int j=0;j<6;j++){
-                if(matrice2[i][j]>Moyenne(matrice2)) 
-		    matrice2[i][j]=50;
-	        else 
-                    matrice2[i][j]=matrice2[i][j]/2;
+                correction=matrice2[i][j]-moy/2;
+                matrice2[i][j]=matrice2[i][j]-correction;
             }
         }
         return matrice2;
